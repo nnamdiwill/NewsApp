69dea499f3454acab2a2eca1036a75fa2108505d
nnamdi <namacctforcicd@gmail.com>
2026-01-04 17:23:24 -0500

    read me file updated

--- app/src/main/java/com/example/news_then_and_now/newsinfo/NewsInfoState.kt
+++ app/src/main/java/com/example/news_then_and_now/newsinfo/NewsInfoState.kt
@@ -3,7 +3,7 @@ package com.example.news_then_and_now.newsinfo
 import com.example.news_then_and_now.dataclasses.CurrentNews
 
 sealed class NewsInfoState {
-
+// updated class files
     data class Loading() : NewsInfoState()
     data class Success(val newsLinks: List<CurrentNews>) : NewsInfoState()
     data class Error(val error: Throwable,val userFriendlyMessageText: String) :NewsInfoState()
