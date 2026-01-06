eb5799900ba1d970c02a35578959a323f7232c91
N'Namdi Williams <nnamdiwill@gmail.com>
2026-01-05 19:28:23 -0500

    Add files via upload
    Module Gradle build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,114 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.compose.compiler) apply false
+    id("kotlin-parcelize")
+   // id("org.jetbrains.kotlin.plugin.compose") version "2.2.21"
+  //  id("com.google.devtools.ksp")
+
+   // id("org.jetbrains.kotlin.android") version "2.2.21" apply false // Update this version
+
 }
 
-buildscript {
-    dependencies {
-        classpath("org.jetbrains.kotlin.plugin.compose:org.jetbrains.kotlin.plugin.compose.gradle.plugin:2.3.0")
+android {
+    namespace = "com.example.news_then_and_now"
+    compileSdk = 36
+
+
+
+    defaultConfig {
+        applicationId = "com.example.news_then_and_now"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+       // sourceCompatibility = JavaVersion.VERSION_17
+      //  targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+      //  kotlinCompilerExtensionVersion = "1.5.1"
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
-composeCompiler {
-    reportsDestination = layout.buildDirectory.dir("compose_compiler")
-    stabilityConfigurationFile = rootProject.layout.projectDirectory.file("stability_config.conf")
+dependencies {
+
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.room.common)
+    implementation(libs.androidx.material3.android)
+    implementation(libs.protolite.well.known.types)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.room.ktx)
+    implementation(libs.androidx.room.common.jvm)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    // https://mvnrepository.com/artifact/com.squareup.retrofit2/retrofit
+    implementation(libs.retrofit)
+    // https://mvnrepository.com/artifact/com.google.dagger/dagger
+    implementation(libs.dagger)
+
+    // https://mvnrepository.com/artifact/com.google.dagger/hilt-core
+    implementation(libs.hilt.core)
+    // https://mvnrepository.com/artifact/androidx.hilt/hilt-navigation-compose
+    // https://mvnrepository.com/artifact/androidx.hilt/hilt-navigation-compose
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation("androidx.preference:preference:1.2.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
+    // https://mvnrepository.com/artifact/org.http4k/http4k-format-moshi
+    implementation(libs.http4k.format.moshi)
+    implementation (libs.converter.moshi)
+    implementation(libs.coil.compose)
+
+    // https://mvnrepository.com/artifact/androidx.room/room-ktx
+    runtimeOnly("androidx.room:room-ktx:2.8.3")
 }
+
